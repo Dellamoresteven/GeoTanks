56559d3fe898d590a1216463afd95b4e8b75abfe
javaComSci <32016612+javaComSci@users.noreply.github.com>
2018-11-17 16:40:44 -0500

    Add dependency for socket.io-client for socket communications

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import socketIOClient from 'socket.io-client'
 
 class App extends Component {
+  constructor() {
+  	super();
+  	this.state = {
+  		// the endpoint I want to connect to
+  		endpoint: "http://192.168.0.10:4001/"
+  	}
+  }
+
+  send = () => {
+  	// function to send emit event to socket
+  	const socket = socketIOClient(this.state.endpoint)
+
+  	// send information to join the game
+  	socket.emit('join game')
+  }
+
   render() {
+  	// check for any sockets
+
     return (
       <div className="App">
       	  <h1 className = "title"> GeoTanks </h1>
