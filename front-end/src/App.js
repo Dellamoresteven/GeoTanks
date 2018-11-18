a1a514a73d78c7b2d5ac08dcfb5d5eb7ebfa4654
javaComSci <32016612+javaComSci@users.noreply.github.com>
2018-11-18 13:01:49 -0500

    Add react-router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,26 +6,34 @@ class App extends Component {
   constructor() {
   	super();
   	this.state = {
-  		// the endpoint I want to connect to
-  		endpoint: "http://192.168.0.10:4001/"
+  		// the endpoint want to connect to for the server
+  		endpoint: "http://192.168.0.10:4001/",
+  		data: "",
   	}
   }
 
-  send = () => {
-  	// function to send emit event to socket
+  sendJoinGame = () => {
+  	// set up socket to emit event
+  	console.log("I WANT TO JOIN GAME!!");
   	const socket = socketIOClient(this.state.endpoint)
-
-  	// send information to join the game
-  	socket.emit('join game')
+  	socket.emit('joinGame', this.state.session)
+  	socket.on('joinGameResponse', (data) => {
+  		console.log("I GOT TO JOIN!" + data)
+  		// need to trigger a rerender with new data
+  		this.setState({data: data})
+  	})
   }
 
   render() {
-  	// check for any sockets
+  	// socket to check for emmissions from other clients
+  	const socket = socketIOClient(this.state.endpoint)
+
+  	console.log("MUST RERENDER" + this.state.data);
 
     return (
       <div className="App">
       	  <h1 className = "title"> GeoTanks </h1>
-      	  <button className = "Button"> Play Game! </button>
+      	  <button onClick = {() => this.sendJoinGame()} className = "Button"> Play Game! </button>
       </div>
     );
   }
