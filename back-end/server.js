6312be5810d342ac5615585b8c5d302df6e926cc
Dellamoresteven <dellamoresteven@gmail.com>
2018-11-24 23:02:49 -0600

    Trying

--- back-end/server.js
+++ back-end/server.js
@@ -50,7 +50,7 @@ const closeDB = () => {
 
 
 app.use(express.static('public'));
-server.listen(process.env.PORT || port, () => console.log(`I'm listening ${rocess.env.PORT}`))
+server.listen(process.env.PORT || port, () => console.log(`I'm listening HERE ${process.env.PORT}`))
 // server.listen(port, () => console.log(`I'm listening ${port}`))
 setInterval(newDrop, 5 * 1000);
 io.on('connect', (socket) => {
