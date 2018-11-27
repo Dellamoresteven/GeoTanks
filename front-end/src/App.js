9262c3751a58f39730ba5a688afe737ca6cdecf0
javaComSci <32016612+javaComSci@users.noreply.github.com>
2018-11-27 09:38:43 -0500

    User preference sending to socket

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,20 +6,21 @@ import Preferences from './Preferences';
 // display game beginning page
 class App extends Component {
   constructor() {
-  	super();
-  	this.state = {
-  		// the endpoint want to connect to for the server
-  		endpoint: window.location.href,
-  		data: "",
+    super();
+    this.state = {
+      // the endpoint want to connect to for the server
+      endpoint: window.location.href,
+      data: "",
       gotoGame: false,
-  	}
+      value: "",
+    }
   }
 
   sendJoinGame = () => {
-  	// set up socket to emit event
-  	console.log("I WANT TO JOIN GAME!!");
+    // set up socket to emit event
   	const socket = socketIOClient(this.state.endpoint)
-  	socket.emit('joinGame', this.state.session)
+    console.log("I WANT TO JOIN GAME!!");
+  	socket.emit('joinGame', this.state.value)
   	socket.on('joinGameResponse', (data) => {
   		console.log("I GOT TO JOIN!" + data)
       // allowed to save the data
@@ -33,9 +34,17 @@ class App extends Component {
   renderRedirect = () => {
     return (
       <div className="App">
-        <Preferences className='Preferences' clicked={{'00':1}}/>
+        <Preferences
+          className='Preferences'
+          playerName={this.state.value}
+          clicked={{'00':1}}/>
       </div>
     );
+  }
+
+  // clicked={{'00':1, '01':1, '02':1,'10':1, '11':1, '12':1, '20':1, '21':1, '22':1}}
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
   }
 
   render() {
@@ -53,6 +62,11 @@ class App extends Component {
     return (
       <div className="App">
       	  <h1 className = "title"> GeoTanks </h1>
+          <div>
+          <label>
+              Name:<input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          </div>
       	  <button onClick = {() => this.sendJoinGame()} className = "Button"> Play Game! </button>
       </div>
     );
