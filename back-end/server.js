f9ec979b34d54b6273c5dc7c1bc6d244e700936a
Dellamoresteven <dellamoresteven@gmail.com>
2018-12-04 18:10:15 -0500

    oops

--- back-end/server.js
+++ back-end/server.js
@@ -148,11 +148,8 @@ io.on('connect', (socket) => {
         if (!(data.TankStatus)) {
             console.log("THE NUMBER O FPLAYER IS " + numPlayers + " " + numSurvivors);
             numSurvivors--;
-<<<<<<< HEAD
-            if (((numPlayers - numSurvivors) == 1) && (numPlayers != 0)) {
-=======
             if (((numPlayers - numSurvivors) == 1) && numPlayers >= 0) {
->>>>>>> b1dd7b2bdf7e6eba8a4901b16c7eb97fa357d6b9
+
                 console.log("GAME ENDING");
                 // take everyone to results page
                 io.sockets.emit('gameDone', allPlayerNames)
